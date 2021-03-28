ec4619250f24f1e2b0ffe4d15a2e77b053227847
Olayiwola Samuel <tolayiwola19@yahoo.com>
2021-03-28 17:44:43 +0100

    update

--- landing/script.js
+++ landing/script.js
@@ -11,7 +11,7 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 5000); 
+  setTimeout(carousel, 5000);
 }
 
 
